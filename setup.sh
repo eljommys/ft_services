1522710582c31d50deae2ad0c3013e12a6b2fa19
j53rran0 <jserrano.gonzalez.moreno@gmail.com>
2020-12-28 18:16:43 +0100

    grafana configuring

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ eval $(minikube docker-env)
 docker build -t my_nginx srcs/nginx
 docker build -t my_mysql srcs/mysql
 docker build -t my_ftps srcs/ftps
+docker build -t my_grafana srcs/grafana
+docker build -t my_influxdb srcs/influxdb
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
@@ -15,6 +17,8 @@ kubectl apply -f srcs/metalLB.yaml
 kubectl apply -f srcs/nginx.yaml
 kubectl apply -f srcs/mysql.yaml
 kubectl apply -f srcs/ftps.yaml
+kubectl apply -f srcs/grafana.yaml
+kubectl apply -f srcs/influxdb.yaml
 
 kubectl create secret generic -n metallb-system memberlist  --from-literal=secretkey="$(openssl rand -base64 128)"
 
