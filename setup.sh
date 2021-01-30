4005e2c90466a7efed3169438abd20855d47cdc8
j53rran0 <jserrano.gonzalez.moreno@gmail.com>
2021-01-30 19:59:14 +0100

    save

--- setup.sh
+++ setup.sh
@@ -25,9 +25,9 @@ kubectl apply -f srcs/nginx.yaml
 kubectl apply -f srcs/ftps.yaml
 kubectl apply -f srcs/grafana.yaml
 kubectl apply -f srcs/influxdb.yaml
+kubectl apply -f srcs/wordpress.yaml
 kubectl apply -f srcs/mysql.yaml
 kubectl apply -f srcs/phpmyadmin.yaml
-kubectl apply -f srcs/wordpress.yaml
 
 minikube addons enable dashboard
 minikube addons enable metrics-server
